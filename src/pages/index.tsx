3513f7e5663dcca170b09e004c10b390626a8eaa
bhenrique16 <bh.henrique16@gmail.com>
2023-10-21 14:52:53 -0300

    Estrutura e estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
-import styles from '../styles/home.module.css'
+import Head from 'next/head'
+
+
 export default function Home() {
   return (
-    <h1 className={styles.title}> hello world</h1>
+    <>
+      <Head>  <title>ig.news</title></Head>
+      <h1 > hello <span>world</span></h1>
+
+    </>
   )
 }
